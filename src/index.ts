c600fb84ed80364406c3aa2d2f68538172226eba
hooksMan <1695415918@qq.com>
2021-06-15 13:22:55 +0800

    feat: package add model

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,25 @@
 /*
  * @Author: 邱狮杰
  * @Date: 2021-05-08 21:20:19
- * @LastEditTime: 2021-05-08 23:21:23
- * @FilePath: /SynchronizationAwaitError/src/index.ts
+ * @LastEditTime : 2021-06-15 13:22:33
+ * @FilePath     : /SynchronizationAwaitError/src/index.ts
  * @Description: SynchronizationAwaitError this is awesome!
  */
 
 /**
  * @description Synchronization capture error
  * @param { Promise } promise Need to catch wrong promise
- * @param { Function } beautifyTeturnValue It's the same as his name
+ * @param { Function } beautifyReturnValue It's the same as his name
  * @param { object }  errorCaptured The error object is added to the catch error and returned together
  * @returns { Promise }
  */
-export function SynchronizationAwaitError<T, B = T, E = Error>(
-  promise: Promise<T>,
-  beautifyTeturnValue?: (res: T) => B,
-  errorCaptured?: object
-): Promise<[E, null] | [null, T | B]> {
+export function SynchronizationAwaitError<T, B = T, E = Error>(promise: Promise<T>, beautifyReturnValue?: (res: T) => B, errorCaptured?: object): Promise<[E, null] | [null, T | B]> {
   return promise
     .then<[null, T | B]>((result: T) => {
-      return [null, beautifyTeturnValue ? beautifyTeturnValue(result) : result];
+      return [null, beautifyReturnValue ? beautifyReturnValue(result) : result]
     })
     .catch<[E, null]>((causeOfError: E) => {
-      if (errorCaptured)
-        return [Object.assign(causeOfError, errorCaptured), null];
-      return [causeOfError, null];
-    });
+      if (errorCaptured) return [Object.assign(causeOfError, errorCaptured), null]
+      return [causeOfError, null]
+    })
 }
