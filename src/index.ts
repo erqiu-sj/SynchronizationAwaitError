557f3408312c06988e6af26394481c3044594d51
hooksMan <1695415918@qq.com>
2021-06-15 13:31:35 +0800

    feat: edit code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 /*
  * @Author: 邱狮杰
  * @Date: 2021-05-08 21:20:19
- * @LastEditTime : 2021-06-15 13:22:33
+ * @LastEditTime : 2021-06-15 13:25:41
  * @FilePath     : /SynchronizationAwaitError/src/index.ts
  * @Description: SynchronizationAwaitError this is awesome!
  */
-
 /**
  * @description Synchronization capture error
  * @param { Promise } promise Need to catch wrong promise
